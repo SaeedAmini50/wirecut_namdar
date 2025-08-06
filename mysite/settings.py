6011c104bbc9e6294b2653a6ffe269586507a324
saeed amini <saeedamini506750@gmail.com>
2025-08-06 14:11:27 +0100

    fix error

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,16 +123,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Media files (Uploaded files)
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 
-STATIC_ROOT=os.path.join(BASE_DIR,'static_cdn')
-MEDIA_ROOT=os.path.join(BASE_DIR,'media_cdn')
-
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static'),
-    os.path.join(BASE_DIR,'media')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
 ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
